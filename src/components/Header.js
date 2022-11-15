9560faaceab96fe16f54eff6ed7589cead5b5c5c
budy6991 <alvmenfer@gmail.com>
2022-11-15 17:59:57 +0100

    Changed colours of characters upon discovering

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ export const Header = ({ time, waldo, odlaw, magician }) => {
   return (
     <div className="w-full h-fit flex justify-between content-center p-7 bg-red-600 items-center">
       <h1 className="text-white text-3xl">Where is Waldo</h1>
-      <div className="flex justify-center gap-28 text-xl text-white italic">
-        <p className={`${waldo ? "line-through" : ""} `}>Waldo</p>
-        <p className={`${odlaw ? "line-through" : ""} `}>Odlaw</p>
-        <p className={`${magician ? "line-through" : ""} `}>Magician</p>
+      <div className="flex justify-center gap-28 text-xl text-red-900 italic">
+        <p className={`${waldo ? "text-green-400 text-2xl" : ""} `}>Waldo</p>
+        <p className={`${odlaw ? "text-green-400 text-2xl" : ""} `}>Odlaw</p>
+        <p className={`${magician ? "text-green-400 text-2xl" : ""} `}>
+          Magician
+        </p>
       </div>
 
       <div className="flex justify-between items-between text-white text-xl">
